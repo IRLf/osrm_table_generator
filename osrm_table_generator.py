96866dc8915cde5252ea2f0981de131a61c08d23
IRLf <33741961+IRLf@users.noreply.github.com>
2020-05-10 14:20:49 -0500

    Improved processing methods, ~8x speed improvement
    Changed to more fully use pandas to speed up file processing.

--- osrm_table_generator.py
+++ osrm_table_generator.py
@@ -1,11 +1,10 @@
 #this is a fork of the code found on https://iliauk.wordpress.com/2016/02/23/millions-of-distances-osrm-python/
-#adapted to linux (specifically ubuntu in the linux for windows subsystem)
-#and for OSRM July 2019 release
-#significant changes done by Rick Fahey
+#and for OSRM May 2020 release
+#changes done by Rick Fahey
 #this is kind of dangerous right now since it doesn't check to see if a server is running
 #nor does it close a server when done
 #this was impractical for me working with a server that loaded 20GB into memory
-#original code was for windows
+#tested only using linux subsystem for windows and linux OSRM server
 
 import os
 import requests
@@ -19,7 +18,6 @@ import sys
 #from multiprocessing import *
 #from subprocess import Popen, PIPE
 
- 
 #initiate the parser 
 parser = argparse.ArgumentParser(description="This script takes in a CSV formatted as ID number (qid), Start Latitude (alat), Start Longitude (alon),\
                                                 End Latitude (blat), End Longitude (blon) and generates an output file with times to the destinations.\
@@ -36,147 +34,74 @@ parser.add_argument("-p", "--port", help="define the port that the server is ope
 parser.add_argument("-d", "--Directory", help="Log directory, default is working directory", type = str, required=False)
 #parser.add_argument("-m", "--Map", help='Location of .OSRM Map file, required if using script to start server', type = str, required=True)
 parser.add_argument("-t", "--transport", help = "Define what transportation method to use for the map. e.g. car", type = str, required = False)
+#parser.add_argument("-c", "--chunks", help = "Define how large of a chunk size to process.", type = int, required = False)
 
 args=parser.parse_args()
 
 if not args.input:
-    print "No input defined"
-    print "Setting input to ./osrm_input.csv"
+    print("No input defined")
+    print("Setting input to ./osrm_input.csv")
     args.input = "./osrm_input.csv"
 else:
-    print "Input file is: {0}" .format(args.input)
+    print(("Input file is: {0}").format(args.input))
+
+#if not args.chunks:
+#    print("No chunk size defined")
+#    print("Setting chunk size to 50,000")
+#    args.chunks = 50000
+#else:
+#    print("Chunk size is: {0}") .format(args.chunks)
 
 if not args.output:
-    print "No output defined"
-    print "Setting output to ./osrm_output.csv"
+    #print("No output defined")
+    print("Setting output to ./osrm_output.csv")
     args.output = "./osrm_output.csv"
+    args.output = None
 else:
-    print "Output file is: {0}" .format(args.output)
+    print(("Output file is: {0}").format(args.output))
 
 if not args.server:
-    print "No server defined"
-    print "Setting server to Localhost"
+    print("No server defined")
+    print("Setting server to Localhost")
     args.server = "localhost"
 else:
-    print "Server located at: {0}" .format(args.server)
+    print("Server located at: {0}" .format(args.server))
 
 if not args.port:
-    print "No port defined"
-    print "Setting port to 5000"
+    print("No port defined")
+    print("Setting port to 5000")
     args.port = 5000
 else:
-    print "Server Port is: {0}" .format(args.port)
+    print("Server Port is: {0}" .format(args.port))
 
 if not args.Directory:
-    print "Working Directory not specified"
-    print "Local Directory will be Used"
+    print("Working Directory not specified")
+    print("Local Directory will be Used")
     args.Directory = "./"
 else:
-    print "Working Directory: {0}" .format(args.Directory)
+    print("Working Directory: {0}" .format(args.Directory))
 
 # if not args.Map:
-    # print "No map file defined"
-    # print "Using default map, map.osm"
+    # print"No map file defined"
+    # print"Using default map, map.osm"
     # args.Map = "map.osm"
 # else:
-    # print "Map: {0}" .format(args.Map)
+    # print"Map: {0}" .format(args.Map)
 
 if not args.transport:
-    print "No Transportation method defined"
-    print "Setting Default to car"
+    print("No Transportation method defined")
+    print("Setting Default to car")
     args.transport = "car"
 else:
-    print "Transportation method: {0}" .format(args.transport)
+    print(("Transportation method: {0}").format(args.transport))
 
- 
-# class OsrmEngine(object):
-    # """ Class which connects to an osrm-routed executable and spawns multiple servers"""
-    # def __init__(self, map_loc, router_loc, max_size):
-        # """
-        # Map needs to be pre-processed with osrm-prepare; router_loc should be the most up to date file from here:
-        # http://build.project-osrm.org/ - both can work over the network with no significant speed-fall as they
-        # are initially loaded into RAM
-        # """
-        # if not os.path.isfile(router_loc):
-            # raise Exception("Could not find osrm-routed executable at: %s" % router_loc)
-        # else:
-            # self.router_loc = router_loc
-        # if not os.path.isfile(map_loc):
-            # raise Exception("Could not find osrm network data at: %s" % map_loc)
-        # else:
-            # self.map_loc = map_loc
-        # #Remove any open instance
-        # if self.server_check():
-            # self.server_kill()
- 
-    # def server_kill(self):
-        # # """
-        # # Kill any osrm-routed server that is currently running before spawning new - this means only one script
-        # # can be run at a time
-        # # """
-        # Popen('taskkill /f /im %s' % os.path.basename(self.router_loc), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        # time.sleep(2)
- 
-    # def server_check(self):
-        # """ 
-        # Check if server is already running
-        # """
-        # try:
-            # if requests.get("http://%s:%d" % (ghost, gport)).status_code == 400:
-                # return True
-        # except requests.ConnectionError:
-            # return False
- 
-    # def server_start(self):
-        # """
-        # Robustness checks to make sure server can be initialised
-        # """
-        
-        # print("Entering Server Start")
-        
-        # try:
-             # p = Popen([self.router_loc, '-v'], stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-             # output = p.communicate()[0].decode("utf-8")
-        # except:
-        # # except FileNotFoundError:
-             # output = ""
-        # if "v" not in str(output):
-            # raise Exception("OSM does not seem to work properly")
-        # # Check server not running
-        # if self.server_check():
-            # raise Exception("osrm-routed already running - force close all with task-manager")
-        # # Start server
-        # Popen("%s %s -i %s -p %d -t %d" % (self.router_loc, self.map_loc, args.server, args.server, cpu_count()), stdout=PIPE, shell=True)
-        # #
-        # #time.sleep(90)
-        # c = 0
-        # while c < 30:
-            # print("Trying to Start Stuff")
-            # try:
-                # if requests.get("http://%s:%d" % (args.server, args.server)).status_code == 400:
-                    # return "http://%s:%d" % (args.server, args.server)
-                # else:
-                    # raise Exception("Map could not be loaded")
-            # except requests.ConnectionError:
-                    # time.sleep(10)
-                    # c += 1
-        # raise Exception("Map could not be loaded ... taking more than 5 minutes..")
- 
-    # def spawn_server(self):
-        # """ Server can handle parallel requests so only one instance needed """
-        # print("------------------starting server----------------")
-        # p = Process(target=self.server_start)
-        # p.start()
-        # p.join()
- 
- 
 # Helper functions
 def process_request(url_input,routelist):
     """
     Submits HTTP request to server and returns distance metrics; errors are coded as status=999
     """
     print("Processing URLs")
-    req_url = url_input
+    #req_url = url_input
     
     routelistlen = len(routelist)
     
@@ -189,38 +114,76 @@ def process_request(url_input,routelist):
     while try_c < 5:
 
         try:
-            response = requests.get(req_url)
+            #response = requests.get(req_url)
+            response = requests.get(url_input)
             json_geocode = response.json()
             status = json_geocode['code']
-            print "Processing JSON file"
+                        
+            #jsonout = open("json_out.txt","w")
+            #jsonout.write(str(json_geocode))
+            #jsonout.close()
+            #print("json file saved")
+            
+            print("Processing JSON file")
+            print("JSON Status: ", status)
             if status in 'Ok':
-            #if True:
+                columns = {'query_id':[0],'total_time':[0],'Start_Longitude':[0], 'Start_Latitude':[0]\
+                           ,'Destination_Longitude':[0],'Destination_Latitude':[0]}
+                
+                #out = pd.DataFrame(data=columns)
+                dict = {}   #making a dictionary to pass stuff to pandas because pandas append is REALLY SLOW
                 for x in tqdm(range(routelistlen)):
                     tot_time_s[x] = json_geocode['durations'][0][x]
                     
+                    if tot_time_s[x] == None:
+                        tot_time_s[x] = 0
+
                     tot_time_m[x] = tot_time_s[x]/60
+
+                    #append method for pandas
+                    dict[x] = {'query_id':x,
+                                      'total_time':tot_time_m[x],
+                                      'Start_Longitude':json_geocode['sources'][0]['location'][0],
+                                      'Start_Latitude':json_geocode['sources'][0]['location'][1],
+                                      'Destination_Longitude':json_geocode['destinations'][x]['location'][0],
+                                      'Destination_Latitude':json_geocode['destinations'][x]['location'][1]}
                     
-                # tot_dist_m = json_geocode['routes'][0]['distance']
-                # #converting the miles calculated
-                # tot_dist_miles = tot_dist_m*0.000621371
-                    #print ("{}% complete".format(((x*100)/(routelistlen-1))))
-                    out[x][0] = routelist.iloc[x]['qid']
-                    out[x][1]= tot_time_m[x]
-                    out[x][2] =json_geocode['sources'][0]['location'][0]
-                    out[x][3] =json_geocode['sources'][0]['location'][1]
-                    out[x][4] =json_geocode['destinations'][x]['location'][0]
-                    out[x][5] =json_geocode['destinations'][x]['location'][1]
+                    #out = out.append({'query_id':x,
+                    #                  'total_time':tot_time_m[x],
+                    #                  'Start_Longitude':json_geocode['sources'][0]['location'][0],
+                    #                  'Start_Latitude':json_geocode['sources'][0]['location'][1],
+                    #                  'Destination_Longitude':json_geocode['destinations'][x]['location'][0],
+                    #                  'Destination_Latitude':json_geocode['destinations'][x]['location'][1]},
+                    #                  ignore_index=True)
+                
+                #reading in out file from dict
+                out = pd.DataFrame.from_dict(dict,"index")
+
+                #removing 0 time points
+                print("Removing 0 Time Locations")
+
+                out = out[out.total_time != 0]
+
+                out = out[['query_id','total_time','Start_Longitude', 'Start_Latitude','Destination_Longitude','Destination_Latitude']]
+                
                 return out
                 # Cannot find route between points (code errors as 999)
             else:
-                return [0, 999, 0, 0, 0, 0, 0, 0]
+                print("Status NOT OK")
+
+                return out
         except Exception as err:
             print("%s - retrying..." % err)
-            print("error should be ^")
+            print("Status Says:",status)
+            print("Iteration: ",x)
+            print("Data from JSON:")
+            print("Latitude: ",out[x-1][5])
+            print("Longitude: ",out[x-1][4])
+            
             time.sleep(5)
             try_c += 1
-    print("Failed: %d %s" % (query_id, req_url))
-    return [query_id, 999, 0, 0, 0, 0, 0, 0]
+    print("Failed: ",x)
+    return out
  
  
 def create_urls(routes, ghost, gport):
@@ -235,13 +198,13 @@ def create_group_url(routelist, ghost, gport):
     path = "table/v1/car/"
 
     waypoints = [[0]*2 for i in range(len(routelist)+1)]
-    waypoints[0][0] = routelist.iloc[0]['alat']
-    waypoints[0][1] = routelist.iloc[0]['alon']
+    waypoints[0][0] = routelist.at[0,'alat']
+    waypoints[0][1] = routelist.at[0,'alon']
     
-    print "Making URL"
+    print("Making URL")
     for x in tqdm(range(len(routelist))):
-        waypoints[x+1][0] = routelist.iloc[x]['blat']
-        waypoints[x+1][1] = routelist.iloc[x]['blon']
+        waypoints[x+1][0] = round(routelist.at[x,'blat'],6)
+        waypoints[x+1][1] = round(routelist.at[x,'blon'],6)
     waypointlist = ';'.join(map(lambda pt: '{},{}'.format(*reversed(pt)), waypoints))
     return "{}{}{}?sources=0".format(host,path,waypointlist)
  
@@ -252,7 +215,7 @@ def loadroute_csv(csv_loc):#, chunks):
     else:
         return pd.read_csv(csv_loc, sep=',')
         
- 
+
 if __name__ == '__main__':
     try:
         stime = time.time()
@@ -266,20 +229,27 @@ if __name__ == '__main__':
             # osrm = OsrmEngine(map_loc, router_loc)
             # print("Loading Map - this may take a while")
             # osrm.spawn_server()
+        directory_loc = args.Directory
         done_count = 0
         print("Loading CSV")
-        with open(os.path.join(directory_loc, args.output), 'w') as outfile:
-            wr = csv.writer(outfile, delimiter=',', lineterminator='\n')
+        #with open(os.path.join(directory_loc, args.output), 'w') as outfile:
+            #wr = csv.writer(outfile, delimiter=',', lineterminator='\n')
             
             #preparing the CSV Columns
-            wr.writerow(['query_id', 'total time', 'Start Longitude', 'Start Latitude', 'Destination Longitude', 'Destination Latitude'])
-            routelist = loadroute_csv(csv_loc=os.path.join(directory_loc, args.input))#, chunks=args.chunks)
-            bigaddress = create_group_url(routelist,args.server,args.server)
-                        
-            calc_routes = process_request(bigaddress,routelist)
-            print("Saving Progress")
-            wr.writerows(calc_routes)
-            dur = time.time() - stime
+            #wr.writerow(['query_id', 'total time', 'Start Longitude', 'Start Latitude', 'Destination Longitude', 'Destination Latitude'])
+        routelist = loadroute_csv(csv_loc=os.path.join(directory_loc, args.input))#, chunks=args.chunks)
+        bigaddress = create_group_url(routelist,args.server,args.port)
+        
+        #bigadd = open("address.txt","w")
+        #bigadd.write(bigaddress)
+        #bigadd.close()
+        
+        calc_routes = process_request(bigaddress,routelist)
+        print(type(calc_routes))
+        print("Saving Progress")
+        calc_routes.to_csv(os.path.join(directory_loc, args.output), index=False)
+
+        dur = time.time() - stime
 
         print("---------------------")
         print("Process Completed")
